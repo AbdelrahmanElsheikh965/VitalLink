8121bd6f433991b0a86cecdb5dde147dc962e262
AbdelrahmanElsheikh965 <abdoelsheikh892@gmail.com>
2023-09-20 15:10:18 +0300

    Enhanced authentication mechanism and modified some migrations

--- routes/api.php
+++ routes/api.php
@@ -25,39 +25,47 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/home', function () {
-    return view('welcome');
-});
 
 # Auth
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class,  'login']);
 Route::post('/reset-password', [AuthController::class, 'resetPassword']);
 Route::post('/create-new-password', [AuthController::class, 'createNewPassword']);
-Route::get('/profile', 'Api\AuthController@profile');
+
+Route::get('cities', [MainController::class, 'cities']);
+Route::get('settings', 'Api\MainController@settings');
+Route::get('categories', 'Api\MainController@categories');
+Route::get('governorates', 'Api\MainController@governorates');
+
 
 Route::group(['middleware'=>'auth:api'], function(){
-    Route::get('cities', [MainController::class, 'cities']);
-    Route::get('settings', 'Api\MainController@settings');
-    Route::get('categories', 'Api\MainController@categories');
-    Route::get('governorates', 'Api\MainController@governorates');
+    # Not yet Finished
+    Route::post('/profile', 'Api\AuthController@profile');
+
+    # Notifications
+    Route::post('/notifications', 'Api\NotificationController@notifications');
+    Route::post('/view-notifications-settings', 'Api\NotificationController@viewNotificationsSettings');
+    
+    // use PUT here
+    Route::post('/update-notifications-settings', 'Api\NotificationController@updateNotificationsSettings');
+
+    # Posts
+    Route::post('/list-favorites', 'Api\PostController@listFavorites');
+    Route::post('/toggle-favorite', 'Api\PostController@toggleFavorite');
+
+    # Donation Requests
+    Route::post('/create-new-donation-request', 'Api\DonationRequestController@createNewDonationRequest');
+
 });
 
-# Notifications
-Route::post('/view-notifications-settings', 'Api\NotificationController@viewNotificationsSettings');
-Route::post('/update-notifications-settings', 'Api\NotificationController@updateNotificationsSettings');
+# Donation Requests
+Route::post('/view-donation-requests', 'Api\DonationRequestController@viewDonationRequests');
+Route::get('/view-one-donation-request/{id}', 'Api\DonationRequestController@viewOneDonationRequest');
 
 # Posts
-Route::post('/toggle-favorite', 'Api\PostController@toggleFavorite');
-Route::post('/list-favorites', 'Api\PostController@listFavorites'); # NOT YET finish.
-Route::get('/view-posts', 'Api\PostController@viewPosts');
+Route::post('/view-posts', 'Api\PostController@viewPosts');
 Route::get('/view-one-post/{id}', 'Api\PostController@viewOnePost');
 
-/*
- * Profile Service    : [get an object from the logged in client/user]
- * */
-
-//Route::apiResources();
 Route::post('/attachClientBloodType', 'Api\NotificationController@attachClientBloodType');
 
 
